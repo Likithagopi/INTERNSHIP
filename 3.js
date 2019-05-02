004389d5fa1382cdacaed1025e7072c3dadbdf30
ѕσσяуα gαηgαяαנ <sooryagangaraj@gmail.com>
2019-05-02 11:36:09 +0530

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,5 +1,4 @@
-
-var mysql = require('mysql');
+var mysql = require("mysql");
 var pool = mysql.createPool({
   host: "remotemysql.com",
   user: "Ybi1BVrtgr",
@@ -7,64 +6,59 @@ var pool = mysql.createPool({
   database: "Ybi1BVrtgr"
 });
 
-
 var http = require("http");
 
 var options = {
-  host: 'www.google.com',
+  host: "www.google.com",
   port: 80,
-  method: 'POST'
+  method: "POST"
 };
-var url='www.google.com';   //url
-console.log('URL:' + url );
+var url = "www.google.com"; //url
+console.log("URL:" + url);
 var req = http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);   //response_code
+  console.log("STATUS: " + res.statusCode); //response_code
 
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    
-  });
+  res.setEncoding("utf8");
+  res.on("data", function(chunk) {});
 });
 
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
+req.on("error", function(e) {
+  console.log("problem with request: " + e.message);
 });
 req.end();
 
-var date = new Date();    //date
-console.log('Date:' +date);
+var date = new Date(); //date
+console.log("Date:" + date);
 
- var response_time = new Date() - date;  //response_time
- console.log('res_time:'  +response_time); 
+var response_time = new Date() - date; //response_time
+console.log("res_time:" + response_time);
 
-var ip_address='192.168.1.18';
- console.log('ip: '  +ip_address); 
+var ip_address = "192.168.1.18";
+console.log("ip: " + ip_address);
 
 //db_connection
- pool.getConnection(function(err, connection) {
-   
-
+pool.getConnection(function(err, connection) {
+  console.log(err);
   let sql =
-      ("INSERT INTO `url_metrics`(`url`, `response_code`, `response_time`,  `ipaddress`, `date`) VALUES (?,?,?,?,?);");
- let post = [
-     url,
-      req,
-      response_time,
-     ip_address,
-      date
-    ];
-    
-    connection.query(sql, post, function(error, results, feild) {
-      console.log(results);
-      connection.release();
+    "INSERT INTO `url_metrics` (`url`, `response_code`, `response_time`, `ipadress`, `date`) VALUES (?, ?, ?, ?, ?);";
 
-   if (error) throw error;
-   });
-     
-    });
+  url = "NULL";
+  req = "NULL";
+  response_time = "NULL";
+  ip_address = "NULL";
+  date = "NULL";
+  let post = [url, req, response_time, ip_address, date];
+  // let post = {
+  //   url: url,
+  //   req: req,
+  //   response_time: response_time,
+  //   ip_address: ip_address,
+  //   date: date
+  // };
 
-
-
-  
-
-
+  connection.query(sql, post, function(error, results, feild) {
+    console.log(results);
+    connection.release();
+    if (error) throw error;
+  });
+});

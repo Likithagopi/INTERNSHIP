36ae53e9e2887ab609c4833547495b97c63373c8
Likithagopi <Likithagopi@github.com>
2019-05-02 11:11:06 +0530

    new bugs

--- server.js
+++ server.js
@@ -1,49 +1,71 @@
-var express = require("express");
-var body_parser = require("body-parser");
-var mysql = require("mysql");
+var express = require('express');
+var body_parser = require('body-parser');
+var mysql      = require('mysql');
 
-let app = express();
-
-app.listen(8080);
-//new fix
-var mysql = require("mysql");
-var pool = mysql.createPool({
-  host: "remotemysql.com",
-  user: "Ybi1BVrtgr",
-  password: "by9clVziwX",
-  database: "Ybi1BVrtgr"
-});
-
-app.get("/*", function(req, res) {
-  var start = new Date();
-
-  var url = req.protocol + "://" + req.get("host") + req.originalUrl;
-  var response_code = "200";
-  var dns_time = start;
-  var ipadress = "127.0.0.1";
-  var date = start;
-  var error = "NULL";
-  var response_time = ".02";
-  pool.getConnection(function(err, connection) {
-    //Time Start for calculating response time
-
-    let sql =
-      "INSERT INTO `url_metrics` (`url`, `response_code`, `response_time`, `dns_time`, `ipadress`, `date`, `error`) VALUES (?,?,?,?,?,?,?);";
-    let post = [
-      url,
-      response_code,
-      response_time,
-      dns_time,
-      ipadress,
-      date,
-      error
-    ];
-    connection.query(sql, post, function(error, results, fields) {
-      console.log(results);
-      connection.release();
-
-      if (error) throw error;
-    });
+//express 
+var app =  express();
+app.listen(8080,function(){
+    console.log('Server started');
+})
+app.all("/*", function(req, res, next) {
+    // we need this to work properly // do not delete this
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    next();
   });
-  res.send(url + " is added to dataBase");
+
+//express module config
+app.use(body_parser.json());
+
+//Database config 
+var connection = mysql.createConnection({
+    host     : 'remotemysql.com',
+    user     : 'Ybi1BVrtgr',
+    password : 'by9clVziwX',
+    database : 'Ybi1BVrtgr'
+  });
+
+//Database connection
+  connection.connect();
+
+// 
+
+
+
+
+//Parsing the get request
+  app.get('/',function(req,res){ 
+      //Time Start for calculating response time
+      var start = new Date();
+
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var response_code = ""
+    var dns_time	=""
+    var ipadress	= req.ip
+    var date	= new Date().getHours();
+    var error=""
+    var response_time	=new Date() - start
+
+    //add these data to database
+    connection.connect();
+    var post  = {"url":url, "response_code":response_code, "dns_time":dns_time, "ipadress":ipadress, "date":date, "error":error, "response_time":response_time,};
+    connection.query('insert into 'url_metrics'('url','response_code','response_time','dns_time','ipadress','date','error') values(?,?,?,?,?,?,?);',post,
+  function (error, results, fields) {
+    // if (error) throw error;
+    console.log('done',results);
+  });
+    
+  console.log(post);
+  
+
+
+    res.send(url + "This url is added to dataBase");
 });
+
+
+
+
